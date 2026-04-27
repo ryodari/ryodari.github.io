675c2a2aa33c7267549ea0baf56bd2ecd8ec941f
ryodari <25200124+ryodari@users.noreply.github.com>
2026-04-27 18:03:35 +0200

    add remark-math and katex

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,20 +4,23 @@ import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
 import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://beparano.id',
   output: 'static',
   markdown: {
+    remarkPlugins: [remarkMath],
     rehypePlugins: [
       rehypeSlug,
       [rehypeAutolinkHeadings, {
-      behavior: 'append',
-      properties: { class: 'heading-anchor', ariaHidden: 'true', tabIndex: -1 },
-      content: { type: 'text', value: '#' },
-    }],
+        behavior: 'append',
+        properties: { class: 'heading-anchor', ariaHidden: 'true', tabIndex: -1 },
+        content: { type: 'text', value: '#' },
+      }],
+      rehypeKatex,
     ],
   },
   vite: {
